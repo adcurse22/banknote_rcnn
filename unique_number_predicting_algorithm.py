2b24557a84b8c963bab1c697501afb50d4f8658b
Aarthur00 <sivolobov.ilya14@mail.ru>
2024-02-04 21:09:09 +0300

    ocr update

--- unique_number_predicting_algorithm.py
+++ unique_number_predicting_algorithm.py
@@ -1,11 +1,12 @@
 import cv2
 import numpy as np
 from tensorflow import keras
+import easyocr
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-DATA_PATH = 'E:/VScodeProjects/banknote_unique_code_cnn/data/1k_3.jpg'
+DATA_PATH = 'data/1k_2.jpg'
 model = keras.models.load_model('banknote_rcnn_2.h5')
 
 #--------------------------BBOX PREDICTING--------------------------
@@ -17,7 +18,7 @@ def bounding_box_transformer(y_pred):
   height1 = y_pred[3] / 2
 
   x2 = x1
-  y2 = height1 / 3 + y1
+  y2 = height1 / 3.2 + y1
   width2 = width1
   height2 = height1
 
@@ -56,3 +57,19 @@ roi = crop_image(img, bbox_predicted)
 #Visualisation
 plt.imshow(cv2.cvtColor(roi, cv2.COLOR_BGR2RGB))
 plt.show()
+
+def text_recognize(img):
+    reader = easyocr.Reader(["ru"])
+    result = reader.readtext(img, detail=1, allowlist='1, 2, 3, 4, 5, 6, 7, 8, 9, 0, А, Б, В, Г, Д, Е, Ё, Ж, З, И, Й, К, Л, М, Н, О, П, Р, С, Т, У, Ф, Х, Ц, Ч, Ш, Щ, Ъ, Ы, Ь, Э, Ю, Я')
+
+    return result
+
+predicted_text = text_recognize(roi)
+predicted_filtered_text = ''
+
+for i in range(len(predicted_text)):
+  
+  if predicted_text[i][-1] >= 0.8:
+    predicted_filtered_text += predicted_text[i][-2]
+    
+print(predicted_filtered_text)
